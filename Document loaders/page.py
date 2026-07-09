e34de034cc644ced484c5e34cfbee4e5da80052c
Puneet Bansal <bpuneet622@gmail.com>
2026-07-09 11:41:38 +0530

    Remove practice folders

--- Document loaders/page.py
+++
@@ -1,9 +0,0 @@
-from langchain_community.document_loaders import WebBaseLoader
-
-url = "https://iiitdwd.ac.in/placements/#placement-statistics"
-
-data = WebBaseLoader(url)
-
-docs = data.load()
-
-print(len(docs))
